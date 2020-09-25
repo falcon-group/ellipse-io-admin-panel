2532056e9fae24e7ce83e1e36b2ec30de2269c72
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2020-09-26 01:43:36 +0300

    Added component Dashboard

---
+++ src/components/dashboard/listItems.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import LayersIcon from "@material-ui/icons/Layers";
+
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Главная" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Пациенты" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Cтатистика" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Настройки" />
+    </ListItem>
+  </div>
+);
+
