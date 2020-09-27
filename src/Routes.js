bdae3d4f2766c1aab8c529f26c6c78a41293adb6
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2020-09-27 17:12:01 +0300

    Added default routing for ./

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PrivateRoute } from 'react-auth-kit'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch,Redirect} from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import SecureComponent from './components/SecureComponent'
@@ -9,10 +9,16 @@ const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path={'/'} component={Home} exact/>
+                <Route exact path="/">
+                    <Redirect to="/login" />
+                </Route>
+
+                {/* <Route path={'/'} component={Home} exact/> */}
                 <Route path={'/login' } component={Login} exact/>
-                <Route path={'/dashboard' } component={Dashboard} exact/>
-                <PrivateRoute path={'/secure'} component={SecureComponent} loginPath={'/login'} exact/>
+                {/* <Route path={'/dashboard' } component={Dashboard} exact/> */}
+                {/* <PrivateRoute path={'/secure'} component={SecureComponent} loginPath={'/login'} exact/> */}
+                <PrivateRoute path={'/dashboard'} component={Dashboard} loginPath={'/login'} exact/>
+
             </Switch>
         </BrowserRouter>
     )
