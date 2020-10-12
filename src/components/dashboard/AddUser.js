cd8fce32c18dfae9a2a16b17306429b1718b38cc
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2020-10-12 15:44:06 +0300

    Minor fixes

--- src/components/dashboard/AddUser.js
+++ src/components/dashboard/AddUser.js
@@ -1,20 +1,41 @@
-
 import React, { useState } from "react";
 import clsx from "clsx";
-import axios from 'axios'
+import axios from "axios";
 //Auth Kit
-import { useAuthUser, useSignOut } from 'react-auth-kit'
+import { useAuthUser, useSignOut } from "react-auth-kit";
 //Library for mask phone number
-import MuiPhoneNumber from 'material-ui-phone-number';
+import MuiPhoneNumber from "material-ui-phone-number";
 //Core MaterialUi
-import { makeStyles,CssBaseline,Switch,Drawer,Box,AppBar,Toolbar,List,
-         Typography,Divider,IconButton,Container,Grid,Paper,Link,TextField,Button,} from "@material-ui/core";
+import {
+  makeStyles,
+  CssBaseline,
+  Switch,
+  Drawer,
+  Box,
+  AppBar,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+  Container,
+  Grid,
+  Paper,
+  Link,
+  TextField,
+  Button,
+} from "@material-ui/core";
 //Icons
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 //Colors
-import {orange,lightBlue,deepPurple, deepOrange} from "@material-ui/core/colors";
+import {
+  orange,
+  lightBlue,
+  deepPurple,
+  deepOrange,
+} from "@material-ui/core/colors";
 //Components
 import { mainListItems } from "./listItems";
 // For Switch Theming
@@ -37,41 +58,41 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex"
+    display: "flex",
   },
   toolbar: {
-    paddingRight: 24 // keep right padding when drawer closed
+    paddingRight: 24, // keep right padding when drawer closed
   },
   toolbarIcon: {
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
     padding: "0 8px",
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
-    marginRight: 36
+    marginRight: 36,
   },
   menuButtonHidden: {
-    display: "none"
+    display: "none",
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   drawerPaper: {
     position: "relative",
@@ -79,35 +100,35 @@ const useStyles = makeStyles(theme => ({
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   drawerPaperClose: {
     overflowX: "hidden",
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
     }),
     width: theme.spacing(7),
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9)
-    }
+      width: theme.spacing(9),
+    },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     height: "100vh",
-    overflow: "auto"
+    overflow: "auto",
   },
   container: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4)
+    paddingBottom: theme.spacing(4),
   },
   paper: {
     padding: theme.spacing(2),
     display: "flex",
     overflow: "auto",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   // fixedHeight: {
   //   height: 240
@@ -118,37 +139,41 @@ export default function AddUser() {
   const [open, setOpen] = useState(true);
   const [darkState, setDarkState] = useState(false);
   const palletType = darkState ? "dark" : "light";
-  const signOut = useSignOut()
-  const authUser = useAuthUser()
+  const signOut = useSignOut();
+  const authUser = useAuthUser();
   const mainPrimaryColor = darkState ? orange[500] : lightBlue[500];
   const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500];
   const darkTheme = createMuiTheme({
     palette: {
       type: palletType,
       primary: {
-        main: mainPrimaryColor
+        main: mainPrimaryColor,
       },
       secondary: {
-        main: mainSecondaryColor
-      }
-    }
+        main: mainSecondaryColor,
+      },
+    },
   });
   const classes = useStyles();
-  const [formData, setFormData] = React.useState({username: '', password: ''})
+  const [formData, setFormData] = React.useState({
+    username: "",
+    password: "",
+  });
 
-  const loginHandler = (e) => {
-    e.preventDefault()
-    formData.username = formData.username.replace(/[^0-9]/g, '');
+  const loginHandler = e => {
+    e.preventDefault();
+    formData.username = formData.username.replace(/[^0-9]/g, "");
     // Assuming that, all network Request is successfull, and the user is authenticated
-    axios.post('https://elepsio.herokuapp.com/auth/register', formData)
-    .then((res)=>{
-        if(res.status === 200){
-         alert("Пользователь добавлен")
-    } else {
-        alert("Error Occoured. Try Again")     
-    }
-  }
-    )}
+    axios
+      .post("https://elepsio.herokuapp.com/auth/register", formData)
+      .then(res => {
+        if (res.status === 200) {
+          alert("Пользователь добавлен");
+        } else {
+          alert("Error Occoured. Try Again");
+        }
+      });
+  };
 
   const handleThemeChange = () => {
     setDarkState(!darkState);
@@ -193,23 +218,20 @@ export default function AddUser() {
               Панель управления
             </Typography>
 
-            
             <Switch checked={darkState} onChange={handleThemeChange} />
 
             <Typography component="h" variant="h8">
-            {`${authUser().name}`}
-                    </Typography>
+              {`${authUser().name}`}
+            </Typography>
             <IconButton color="inherit" onClick={() => signOut()}>
-              < ExitToAppIcon />
+              <ExitToAppIcon />
             </IconButton>
-            
-            
           </Toolbar>
         </AppBar>
         <Drawer
           variant="permanent"
           classes={{
-            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
           }}
           open={open}
         >
@@ -227,59 +249,55 @@ export default function AddUser() {
         <main className={classes.content}>
           <div className={classes.appBarSpacer} />
           <Container maxWidth="lg" className={classes.container}>
-
             <Grid container spacing={3}>
-              <Grid item xs={12} md={8} lg={5}>
+              <Grid item xs={6}>
                 <Paper className={fixedHeightPaper}>
-                <form className={classes.form} onSubmit={loginHandler} >
-
-                <Typography component="h1" variant="h6">
-                   Регистрация нового пациента
+                  <form className={classes.form} onSubmit={loginHandler}>
+                    <Typography component="h1" variant="h6">
+                      Регистрация нового пациента
                     </Typography>
 
-                                  <MuiPhoneNumber
-                  defaultCountry={'ua'}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Имя"
-                  name="username"
-                  autoComplete="username"
-                  autoFocus
-                  onChange ={(e)=>setFormData({...formData, username: e})}
-              />
+                    <MuiPhoneNumber
+                      defaultCountry={"ua"}
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="username"
+                      label="Имя"
+                      name="username"
+                      autoComplete="username"
+                      autoFocus
+                      onChange={e => setFormData({ ...formData, username: e })}
+                    />
                     <TextField
-                        onChange = {(e)=>setFormData({...formData, password: e.target.value})}
-
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Пароль"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-
+                      onChange={e =>
+                        setFormData({ ...formData, password: e.target.value })
+                      }
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Пароль"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
                     />
 
                     <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
                     >
-                        Регистрация
+                      Регистрация
                     </Button>
-
-                    </form>
+                  </form>
                 </Paper>
               </Grid>
             </Grid>
-            
             <Box pt={4}>
               <Copyright />
             </Box>
