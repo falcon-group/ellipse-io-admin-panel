c385b6ca4380c2135c17c79b8c3d0d4a4995f410
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2020-10-16 11:39:24 +0300

    Delete home component and route

--- src/Routes.js
+++ src/Routes.js
@@ -1,31 +1,41 @@
-import React from 'react'
-import { PrivateRoute } from 'react-auth-kit'
-import { BrowserRouter, Route, Switch,Redirect} from 'react-router-dom'
-import Login from './components/Login'
-import Dashboard from './components/dashboard/Dashboard'
-import UsersList from './components/dashboard/UsersList'
-import AddUser from './components/dashboard/AddUser'
-import NotesUser from './components/dashboard/NotesUser'
-
+import React from "react";
+import { PrivateRoute } from "react-auth-kit";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import UsersList from "./components/dashboard/UsersList";
+import AddUser from "./components/dashboard/AddUser";
+import NotesUser from "./components/dashboard/NotesUser";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/">
-                    <Redirect to="/login" />
-                </Route>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
 
-                <Route path={'/login' } component={Login} exact/>
-                <PrivateRoute path={'/dashboard'} component={Dashboard} loginPath={'/login'} exact/>
-                <PrivateRoute path={'/users'} component={UsersList} loginPath={'/login'} exact/>
-                <PrivateRoute path={'/add-user'} component={AddUser} loginPath={'/login'} exact/>
-                <PrivateRoute path={'/notes-user/:id'} component={NotesUser} loginPath={'/login'} exact/>
+        <Route path={"/login"} component={Login} exact />
+        <PrivateRoute
+          path={"/users"}
+          component={UsersList}
+          loginPath={"/login"}
+          exact
+        />
+        <PrivateRoute
+          path={"/add-user"}
+          component={AddUser}
+          loginPath={"/login"}
+          exact
+        />
+        <PrivateRoute
+          path={"/notes-user/:id"}
+          component={NotesUser}
+          loginPath={"/login"}
+          exact
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-
-            </Switch>
-        </BrowserRouter>
-    )
-}
-
-export default Routes
+export default Routes;
