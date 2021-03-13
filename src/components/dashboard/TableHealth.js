f34087f814e3ba73fb997aa918ed03ba932c0390
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2021-03-13 13:43:23 +0200

    Added request for get helth params and  displaying them in a table.

--- src/components/dashboard/TableHealth.js
+++ src/components/dashboard/TableHealth.js
@@ -1,107 +1,3 @@
-// import React from "react";
-// import Link from "@material-ui/core/Link";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Table from "@material-ui/core/Table";
-// import TableBody from "@material-ui/core/TableBody";
-// import TableCell from "@material-ui/core/TableCell";
-// import TableHead from "@material-ui/core/TableHead";
-// import TableRow from "@material-ui/core/TableRow";
-// import Title from "../interface/TittleNav";
-
-// // Generate Order Data
-// function createData(id, date, name, shipTo, paymentMethod, amount) {
-//   return { id, date, name, shipTo, paymentMethod, amount };
-// }
-
-// const rows = [
-//   createData(
-//     0,
-//     "16 Mar, 2019",
-//     "Elvis Presley",
-//     "Tupelo, MS",
-//     "VISA ⠀•••• 3719",
-//     312.44
-//   ),
-//   createData(
-//     1,
-//     "16 Mar, 2019",
-//     "Paul McCartney",
-//     "London, UK",
-//     "VISA ⠀•••• 2574",
-//     866.99
-//   ),
-//   createData(
-//     2,
-//     "16 Mar, 2019",
-//     "Tom Scholz",
-//     "Boston, MA",
-//     "MC ⠀•••• 1253",
-//     100.81
-//   ),
-//   createData(
-//     3,
-//     "16 Mar, 2019",
-//     "Michael Jackson",
-//     "Gary, IN",
-//     "AMEX ⠀•••• 2000",
-//     654.39
-//   ),
-//   createData(
-//     4,
-//     "15 Mar, 2019",
-//     "Bruce Springsteen",
-//     "Long Branch, NJ",
-//     "VISA ⠀•••• 5919",
-//     212.79
-//   ),
-// ];
-
-// function preventDefault(event) {
-//   event.preventDefault();
-// }
-
-// const useStyles = makeStyles(theme => ({
-//   seeMore: {
-//     marginTop: theme.spacing(3),
-//   },
-// }));
-
-// export default function TableHealth() {
-//   const classes = useStyles();
-//   return (
-//     <React.Fragment>
-//       <Title>Recent Orders</Title>
-//       <Table size="small">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Date</TableCell>
-//             <TableCell>Name</TableCell>
-//             <TableCell>Ship To</TableCell>
-//             <TableCell>Payment Method</TableCell>
-//             <TableCell align="right">Sale Amount</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map(row => (
-//             <TableRow key={row.id}>
-//               <TableCell>{row.date}</TableCell>
-//               <TableCell>{row.name}</TableCell>
-//               <TableCell>{row.shipTo}</TableCell>
-//               <TableCell>{row.paymentMethod}</TableCell>
-//               <TableCell align="right">{row.amount}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//       <div className={classes.seeMore}>
-//         <Link color="primary" href="#" onClick={preventDefault}>
-//           See more orders
-//         </Link>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
-
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
@@ -120,15 +16,21 @@ import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import LastPageIcon from "@material-ui/icons/LastPage";
 import TableHead from "@material-ui/core/TableHead";
 import Title from "../interface/TittleNav";
+import { useParams } from "react-router";
+import axios from "axios";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { ruRU } from "@material-ui/core/locale";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({}, ruRU);
 const useStyles1 = makeStyles(theme => ({
   root: {
     flexShrink: 0,
     marginLeft: theme.spacing(2.5),
   },
 }));
-
-function TablePaginationActions(props) {
+const TablePaginationActions = props => {
   const classes = useStyles1();
   const theme = useTheme();
   const { count, page, rowsPerPage, onChangePage } = props;
@@ -189,7 +91,7 @@ function TablePaginationActions(props) {
       </IconButton>
     </div>
   );
-}
+};
 
 TablePaginationActions.propTypes = {
   count: PropTypes.number.isRequired,
@@ -198,20 +100,6 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat, data) {
-  return { name, calories, fat, data };
-}
-
-const rows = [
-  createData("18.24", 76, 4532, 254),
-  createData("18.24", 32, 4534, 532),
-  createData("18.24", 32, 4534, 532),
-  createData("18.24", 32, 4534, 532),
-  createData("18.24", 32, 4534, 532),
-  createData("18.24", 32, 4534, 532),
-  createData("18.24", 65, 3452, 562),
-];
-
 const useStyles2 = makeStyles({
   table: {
     minWidth: 500,
@@ -219,9 +107,28 @@ const useStyles2 = makeStyles({
 });
 
 export default function TableHealth() {
+  const [rows, setRows] = React.useState([]);
+
+  const { customId } = useParams();
+  // console.log(customId);
+
+  const authToken = Cookies.get("_auth_t");
+  axios.interceptors.request.use(config => {
+    config.headers.authorization = ` ${authToken}`;
+    return config;
+  });
+
+  useEffect(() => {
+    // GET request using axios inside useEffect React hook
+    axios
+      .get(
+        `https://elepsio.herokuapp.com/api/admin/health_params?page=1&perPage=300&userCustomId=${customId}`
+      )
+      .then(result => setRows(result.data));
+  }, []);
   const classes = useStyles2();
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
@@ -245,8 +152,6 @@ export default function TableHealth() {
             <TableRow>
               <TableCell>Время</TableCell>
               <TableCell>Пульс</TableCell>
-              <TableCell>Пройденные шаги</TableCell>
-              <TableCell>Калории</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -256,11 +161,9 @@ export default function TableHealth() {
             ).map(row => (
               <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {new Date(row.createDate).toLocaleString()}
                 </TableCell>
-                <TableCell style={{ width: 160 }}>{row.calories}</TableCell>
-                <TableCell style={{ width: 160 }}>{row.fat}</TableCell>
-                <TableCell style={{ width: 160 }}>{row.data}</TableCell>
+                <TableCell style={{ width: 160 }}>{row.heartRate}</TableCell>
               </TableRow>
             ))}
 
@@ -272,20 +175,24 @@ export default function TableHealth() {
           </TableBody>
           <TableFooter>
             <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                colSpan={3}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { "aria-label": "rows per page" },
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
+              <ThemeProvider theme={theme}>
+                <TablePagination
+                  // labelRowsPerPage={"Строк на странице:"}
+
+                  rowsPerPageOptions={[25, 10, 5, { label: "Все", value: -1 }]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: { "aria-label": "количество строк" },
+                    native: true,
+                  }}
+                  onChangePage={handleChangePage}
+                  onChangeRowsPerPage={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </ThemeProvider>
             </TableRow>
           </TableFooter>
         </Table>
